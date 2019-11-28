cfc7b46dda7d8b53c3fc73b0840035b8b49fa824
Ilyass Khadem <ikhadem@e2r1p3.1337.ma>
2019-11-28 19:51:18 +0100

    fixed while loop

--- srcs/start.sh
+++ srcs/start.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 
-service php7.3-fpm start && service nginx start && service mysql start && while true; do sleep 1; done
+service php7.3-fpm start && service mysql start && service nginx start && tail -f /var/log/nginx/access.log
